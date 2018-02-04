7623af43ee2ac1ecdd5fde0e19362915d0cd28d4
rob ashton <ashtonr132@gmail.com>
2018-02-04 12:49:34 +0000

    ui work

---
+++ Assets/Scripts/ScalePos.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScalePos : MonoBehaviour {
+    GameObject PauseScreen, InstructionsText, TipsText, BarUI, StartText, EndText;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+}
