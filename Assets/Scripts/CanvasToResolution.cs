dcdcff9d20e20ade4d5b13f5a1fc492d030c5e83
rob ashton <ashtonr132@gmail.com>
2018-02-04 22:21:55 +0000

    game pretty much complete

---
+++ Assets/Scripts/CanvasToResolution.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasToResolution : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float currentAspect = ((float)Screen.width / (float)Screen.height) / 5;
+        Camera.main.orthographicSize = (Screen.currentResolution.height / currentAspect) / 200;
+    }
+}
